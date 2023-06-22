1d9a937b6ba9b12b6ec7171abcd45a446e53d6f3
qynx <37181043+qynx@users.noreply.github.com>
2023-06-22 10:46:55 +0000

    Deploying to gh-pages from @ qynx/qynx.github.io@a772c0b715521121d3e1a4a6a9a5a5b99ff5309e 🚀

---
+++ assets/gradle.html-e1910481.js
@@ -0,0 +1,6 @@
+import{_ as a,W as n,X as s,a0 as t,Y as e,Z as i,a2 as r}from"./framework-3ac60b7d.js";const d={},c=e("h1",{id:"maven-镜像配置",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#maven-镜像配置","aria-hidden":"true"},"#"),i(" maven 镜像配置")],-1),l=r(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>allprojects {
+    repositories {
+        maven { url &#39;https://maven.aliyun.com/repository/public/&#39; }
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function o(m,v){return n(),s("div",null,[c,t(" more "),l])}const _=a(d,[["render",o],["__file","gradle.html.vue"]]);export{_ as default};

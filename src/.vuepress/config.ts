99436a992bb186d1a40c451838735d434e199ecf
zsl <zslngu@163.com>
2023-10-01 17:39:40 +0800

    hero text adjust

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,7 +7,7 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",
-      title: "viper3's blog",
+      title: "悟已往之不谏，知来者之可追",
       description: "viper3's blog",
     },
     "/en/": {

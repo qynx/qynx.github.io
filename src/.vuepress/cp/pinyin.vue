a810ca577fa436e77fcfa88d439d16873ff7daca
zsl <zslngu@163.com>
2023-10-02 17:40:08 +0800

    增加拼音组件

---
+++ src/.vuepress/cp/pinyin.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {defineProps} from 'vue'
+
+const props = defineProps({
+    "text":{},
+    "title": {}
+})
+
+</script>
+
+<template>
+<el-popover  :title='props.title' trigger='hover'>
+    <template #reference>
+        <span style='border-bottom: 2px solid green'>{{ props.text }}</span>
+    </template>
+</el-popover>
+</template>

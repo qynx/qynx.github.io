b94b039457f2d965bc7892ec57d5ba2e7611afd1
zsl <zslngu@163.com>
2023-10-02 21:09:51 +0800

    浪淘沙_帘外雨潺潺

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -3,10 +3,13 @@ import { defineClientConfig } from "@vuepress/client";
 import Element from 'element-plus';
 import 'element-plus/dist/index.css'
 import pinyin from './cp/pinyin.vue'
+import naive from 'naive-ui'
+
 
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
     app.use(Element)
+    app.use(naive)
     app.component("pinyin", pinyin)
   },
 });

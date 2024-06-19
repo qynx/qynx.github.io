a8105447eb5f664e196f018881da8f2749808d94
zsl <zslngu@163.com>
2024-06-20 00:56:30 +0800

    更新说明引用

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -15,7 +15,7 @@ export const zhNavbar = navbar([
         children: [
           { text: "相见欢-李煜", icon: "alias", link: "赵宋/相见欢_林花谢了春红" },
           { text: "锦瑟-李商隐", icon: "alias", link: "李唐/锦瑟" },
-          { text: "临江仙", icon: "alias", link: "明/临江仙_杨慎" },
+          { text: "临江仙-杨慎", icon: "alias", link: "明/临江仙_杨慎" },
         ],
       }
       //{ text: "樱桃", icon: "edit", link: "cherry" },

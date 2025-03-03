b83b24436046a2c7072ec7f028223cce7abcee23
zsl <zslngu@163.com>
2025-03-04 01:51:14 +0800

    目录时间线改用antd

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,16 +1,17 @@
 // .vuepress/client.ts
 import { defineClientConfig } from "vuepress/client";
-import Element from 'element-plus';
-import 'element-plus/dist/index.css'
 import pinyin from './cp/pinyin.vue'
 import SblgRate from './cp/SblgRate.vue'
 import naive from 'naive-ui'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
 
 
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
-    app.use(Element)
     app.use(naive)
+    app.use(Antd)
     app.component("pinyin", pinyin)
     app.component("sblg-rate", SblgRate)
   },

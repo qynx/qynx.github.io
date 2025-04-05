148e39f470397b0259567e7da067b17296a66297
zsl <zslngu@163.com>
2025-04-06 00:03:17 +0800

    南京游记2025

--- src/.vuepress/cp/SblgRate.vue
+++ src/.vuepress/cp/SblgRate.vue
@@ -4,11 +4,15 @@ import {defineProps, ref} from 'vue'
 const props = defineProps({
     "rate": {
 
-    }
+    },
+    "desc":{}
 })
 const colors = ref(['#99A9BF', '#F7BA2A', '#FF9900'])
 </script>
 
 <template>
-<el-rate v-model="props.rate" :colors="colors" ></el-rate>
+<span>
+    <a-rate v-model:value="props.rate" :colors="colors" ></a-rate>
+    <span class="ant-rate-text">{{ desc || "" }}</span>
+</span>
 </template>
